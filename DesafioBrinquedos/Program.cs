127d15a2f8a3ae4ddfcfbf112b317aae2acab1fd
Miguel Freire <72529654+MiguelFreire-bit@users.noreply.github.com>
2021-09-12 15:31:18 -0300

    Add files via upload

---
+++ DesafioBrinquedos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MinhaClasse {
+  public static void Main (string[] args) 
+  {
+    string[] line1 = Console.ReadLine().Split(" ");
+    string[] line2 = Console.ReadLine().Split(" ");
+    
+    int finalExpediente = int.Parse(line1[0]);
+    
+    int presente1 = int.Parse(line2[0]);
+    int presente2 = int.Parse(line2[1]);
+    
+    int totalNecessario = presente1 + presente2;
+    
+    if ( finalExpediente < totalNecessario)
+    {
+      Console.WriteLine("Deixa para amanha!");
+    }
+    else
+    {
+      Console.WriteLine("Farei hoje!");
+    }
+  }
+}
